ebb07c3b3028ae472ba36635454c2ab5eab5fd1b
otobongfp <otobongfp@gmail.com>
2025-05-26 10:59:58 +0100

    transfer support for ownables via the cli

--- bin/ownables-cli.js
+++ bin/ownables-cli.js
@@ -1,49 +1,27 @@
 #!/usr/bin/env node
 
-const { program } = require("commander");
-const chalk = require("chalk");
+const { Command } = require("commander");
 const { create } = require("../lib/commands/create");
-const { build, clean } = require("../lib/commands/build");
+const { build } = require("../lib/commands/build");
+const { transfer } = require("../lib/commands/transfer");
+
+const program = new Command();
 
 program
-  .name("ownables-cli")
-  .description("CLI tool for creating and building Ownables")
+  .name("ownables")
+  .description("CLI for creating and building Ownables")
   .version("1.0.0");
 
 program
   .command("create")
-  .description("Create a new Ownable template in the current directory")
-  .action(async () => {
-    try {
-      await create();
-    } catch (error) {
-      console.error(chalk.red("Error:"), error.message);
-      process.exit(1);
-    }
-  });
+  .description("Create a new Ownable project")
+  .action(create);
+
+program.command("build").description("Build an Ownable project").action(build);
 
 program
-  .command("build")
-  .description("Build the Ownable project")
-  .action(async () => {
-    try {
-      await build();
-    } catch (error) {
-      console.error(chalk.red("Error:"), error.message);
-      process.exit(1);
-    }
-  });
-
-program
-  .command("clean")
-  .description("Clean build cache (build and target directories)")
-  .action(async () => {
-    try {
-      await clean(process.cwd());
-    } catch (error) {
-      console.error(chalk.red("Error:"), error.message);
-      process.exit(1);
-    }
-  });
+  .command("transfer")
+  .description("Transfer an Ownable package to a recipient via LTO Relay")
+  .action(transfer);
 
 program.parse();
