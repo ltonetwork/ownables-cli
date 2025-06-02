276429cc7a2b5f7f0f9a16df3e5b996960e902c2
otobongfp <otobongfp@gmail.com>
2025-06-02 14:07:19 +0100

    Allow multi-transfer

---
+++ constants.js
@@ -0,0 +1,8 @@
+const LTO_FEE = 0.5;
+const RELAY_URL = "https://relay-dev.lto.network";
+const TESTNET_EXPLORER_URL = "https://explorer.testnet.lto.network";
+const MAINNET_EXPLORER_URL = "https://explorer.lto.network";
+const MAINNET_API_URL = "https://nodes.lto.network";
+const TESTNET_API_URL = "https://testnet.lto.network";
+
+module.exports = { LTO_FEE, RELAY_URL, TESTNET_EXPLORER_URL, MAINNET_EXPLORER_URL, MAINNET_API_URL, TESTNET_API_URL }; 
